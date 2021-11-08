47e5acd342a97181f91ce1a7d894a675ade37eb6
gaibinus <filip9geib@gmail.com>
2021-11-08 18:08:03 +0100

    mac masking changed

--- examples/mowi_wifi_basic/main/mowi_wifi_basic.c
+++ examples/mowi_wifi_basic/main/mowi_wifi_basic.c
@@ -116,8 +116,8 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t ev
         break;
 
     case ETHERNET_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "Ethernet Link Down");
-        eth_mac_is_set = false;
+        ESP_LOGE(TAG, "Ethernet Link Down");
+        esp_restart();
         break;
 
     case ETHERNET_EVENT_START:
@@ -269,6 +269,11 @@ static void initialize_ethernet(void)
     while(!eth_mac_is_set) {
         vTaskDelay(10);
     }
+
+    // Mask ESP MAC addres with Mosaic one
+    ESP_ERROR_CHECK(esp_base_mac_addr_set(eth_mac));
+    ESP_LOGI(TAG, "ESP MAC address set as: %02X:%02X:%02X:%02X:%02X:%02X", 
+        eth_mac[0], eth_mac[1], eth_mac[2], eth_mac[3], eth_mac[4], eth_mac[5]);
 }
 
 static void initialize_wifi(void)
@@ -284,7 +289,6 @@ static void initialize_wifi(void)
     esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_mac(WIFI_IF_STA, eth_mac));
 
     // Create WiFi config
     wifi_config_t wifi_config = {
